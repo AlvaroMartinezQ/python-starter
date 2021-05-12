c10444275d9ab4c84259affefe95c95a5e5a70a2
Alvaro Martinez <alvaroo2302@gmail.com>
2021-05-12 12:42:53 +0200

    Feature: config file reader

--- codelab/availability_detector/config_reader.py
+++ codelab/availability_detector/config_reader.py
@@ -2,12 +2,22 @@
 
 from configparser import ConfigParser
 
-def read_properties():
+def read_propertie():
     parser = ConfigParser()
     parser.read('.env')
 
     print (parser.get('mail_creds', 'email_host'))
 
+def read_properties():
+    parser = ConfigParser()
+    parser.read('.env')
+
+    data = {}
+    data["email"] = parser.get('mail_creds', 'email_host')
+    data["passw"] = parser.get('mail_creds', 'email_password')
+    data["port"] = parser.get('mail_creds', 'email_host_port')
+    print(data)
+    return data
 
 if __name__ == '__main__':
     read_properties()
