a54f465a2bab9d86a3143963b650167e80cbe759
Alvaro Martinez <alvaroo2302@gmail.com>
2021-05-19 11:54:59 +0200

    Feature: mentions remove

--- codelab/tweet_emotions/utils.py
+++ codelab/tweet_emotions/utils.py
@@ -16,8 +16,25 @@ def rm_hashtags(str_in, hashes = False):
             str_out += word
             str_out += ' '
 
+    # print(str_out)
+    return str_out
+
+def rm_mentions(str_in, names = False):
+    str_inter = str(str_in)
+    str_inter = str_inter.split(" ")
+    str_out = ''
+    for word in str_inter:
+        if word[0] == '@':
+            # Maybe store the names for a future user check
+            pass
+        else:
+            str_out += word
+            str_out += ' '
+
     print(str_out)
     return str_out
 
 if __name__ == '__main__':
-    rm_hashtags("hola que tal estas #buenosdias")
+    pass
+    # rm_hashtags("good morning how are yall #goodmorning")
+    # rm_mentions("hey wasssup man @alvaro")
