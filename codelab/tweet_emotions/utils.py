b3dc08eeb107ca2b0a77b7377449a31b02be0020
Alvaro Martinez <alvaroo2302@gmail.com>
2021-05-19 13:28:14 +0200

    Feature: tokenizer

--- codelab/tweet_emotions/utils.py
+++ codelab/tweet_emotions/utils.py
@@ -49,8 +49,14 @@ def rm_points_digits(str_in):
     # print(str_out)
     return str_out
 
+def tokenize_str(str_in):
+    arr_out = str_in.split(" ")
+    # print(arr_out)
+    return arr_out
+
 if __name__ == '__main__':
     pass
     # rm_hashtags("good morning how are yall #goodmorning")
     # rm_mentions("hey wasssup man @alvaro")
     # rm_points_digits("this is a test string ok 1234 wow 1233 2323 yes 2..")
+    # tokenize_str("Let's deploy on friday LOL!!")
