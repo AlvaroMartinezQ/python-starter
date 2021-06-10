08620491f36d8c6c0edf98587359be9e590a443a
Alvaro Martinez <alvaroo2302@gmail.com>
2021-06-10 13:01:56 +0200

    Feature: my zip program

--- module_1-exercises/my_zip.py
+++ module_1-exercises/my_zip.py
@@ -1,3 +1,17 @@
 # Simulates zip
-tuples = tuple(input("Please input your tuples as: [(1,2,3), (a,b,c)] "))
-print(tuples)
+def my_zip(*args):
+
+    min_length = min(map (len, args)) # gets the tuple with least values
+
+    result = []
+
+    for i in range(min_length):
+        elem = [sequence[i] for sequence in args]
+        print(elem)
+        result.append(tuple(elem))
+
+    return result
+
+if __name__ == "__main__":
+    print(my_zip([10, 20,30], 'abcd'))
+
